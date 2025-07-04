7e0b3b38b3deebf07da2ce8ed802543063b5cfc9
sameerjena <sameerjena@zkdigimax.com>
2025-07-04 16:17:22 +0530

    importnat session issue fixed

--- database/migrations/0001_01_01_000006_create_users_table.php
+++ database/migrations/0001_01_01_000006_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration {
     /**
      * Run the migrations.
      */
@@ -44,11 +44,15 @@ return new class () extends Migration {
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
+            $table->uuid('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
             $table->integer('last_activity')->index();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('set null'); // Changed from cascade to set null
         });
     }
 
